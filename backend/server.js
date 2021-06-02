83f281eb37f64d7ef383317bab6af5110185ba6e
Kiah Pleasant <kiahpleasant@gmail.com>
2021-06-02 15:03:28 -0400

    first commit

---
+++ backend/server.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+
+//creating express server connecting 
+const express = require('express');
+const cors = require('cors');
+
+//require mongoose
+const mongoose = require('mongoose');
+
+
+
+//attaching cors and express json middleware
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+//connecting database
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
+);
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+})
+
+//tells server to use the files in the route folder
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+//added as middleware
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
+
+
+//server listening on port 
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
---
+++ src/components/create-user.component.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import gym3 from '../img/gym3.jpg';
+
+export default class CreateUser extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = {
+      username: ''
+    }
+  }
+
+  onChangeUsername(e) {
+    this.setState({
+      username: e.target.value
+    })
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const user = {
+      username: this.state.username
+    }
+
+    console.log(user);
+
+    axios.post('http://localhost:5000/users/add', user)
+      .then(res => console.log(res.data));
+
+    this.setState({
+      username: ''
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Create New User</h3>
+
+      <div className="big-div">
+      <div className=" big-img ">
+       <img src={gym3} alt="man running"  className="big-img2"/>
+      </div>
+
+
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group"> 
+            <label>Username: </label>
+            <input  type="text"
+                required
+                className="form-control"
+                value={this.state.username}
+                onChange={this.onChangeUsername}
+                />
+          </div>
+          <div className="form-group">
+            <input type="submit" value="Create User" className="btn " />
+          </div>
+        </form>
+      </div>
+      </div>
+    )
+  }
+}
